1683426a0dce2a9919c0650b77c0cb51bbde0bf0
mneckoska <mneckoska@yahoo.com>
2021-02-21 14:59:27 +0100

    third commit

--- script.js
+++ script.js
@@ -1,82 +1,97 @@
+var count = 0;
+var amount = 4;
 function loadMore() {
-  var pomArray = data.slice(0,4);
+  var pomArray = data.slice(count, amount);
+  if (count == data.length) {
+    document.getElementById("btnLoad").style.display = "none";
+  }
   for (i = 0; i < pomArray.length; i = i + 1) {
-   
     var divColumn = document.createElement("div");
     divColumn.className = "column";
+    divColumn.setAttribute("onclick", "modalClick(this)");
+
     var pomDiv = document.createElement("div");
-    pomDiv.className="images"; 
+    pomDiv.className = "header";
 
     var profileImg = document.createElement("img");
     profileImg.setAttribute("src", pomArray[i].profile_image);
     profileImg.setAttribute("class", "profile");
-   // divColumn.append(profileImg);
     var pName = document.createElement("p");
     pName.innerHTML = pomArray[i].name;
     pName.className = "name";
-   // divColumn.append(pName);
-
-  
-
 
     var pDate = document.createElement("p");
     splitedDate = pomArray[i].date.split(" ");
     const dateMonth = new Date(splitedDate[0]);
-    split=splitedDate[0].split("-");
+    split = splitedDate[0].split("-");
     const month = dateMonth.toLocaleString("default", { month: "long" });
-    pDate.innerHTML = split[2]+" "+ month+ " " +split[0];
+    pDate.innerHTML = split[2] + " " + month + " " + split[0];
     pDate.className = "date";
-    //divColumn.append(pDate);
-    var logo = document.createElement("img");
-    if(pomArray[i].source_type =="instagram")
-    {
-    logo.setAttribute("src","icons/instagram-logo.svg");
-    }
-    else{
-        logo.setAttribute("src","icons/facebook.svg");
-    }
-    
-   logo.className="insta";
 
+    var logo = document.createElement("img");
+    if (pomArray[i].source_type == "instagram") {
+      logo.setAttribute("src", "icons/instagram-logo.svg");
+    } else {
+      logo.setAttribute("src", "icons/facebook.svg");
+    }
+
+    logo.className = "insta";
 
     pomDiv.append(profileImg);
     pomDiv.append(pName);
-    pomDiv.append(pDate)
     pomDiv.append(logo);
     divColumn.append(pomDiv);
-    //divColumn.append(pDate);
+    divColumn.append(pDate);
 
-   // divColumn.append(logo);
-    var image=document.createElement("img");
-    image.setAttribute("src",pomArray[i].image);
-    image.className="imageClass";
-    image.setAttribute("onclick","imgClick(this)")
+    var image = document.createElement("img");
+    image.setAttribute("src", pomArray[i].image);
+    image.className = "imageClass";
     divColumn.append(image);
     var pCaption = document.createElement("p");
-    pCaption.className="caption";
-    pCaption.innerHTML=pomArray[i].caption;
+    pCaption.className = "caption";
+    pCaption.innerHTML = pomArray[i].caption;
     divColumn.append(pCaption);
     var lastDiv = document.createElement("div");
-    lastDiv.className="likes";
+    lastDiv.className = "likes";
     var icon = document.createElement("img");
-    icon.setAttribute("src","icons/heart.svg");
-    //divColumn.append(icon);
+    icon.className = "hearts";
+    icon.setAttribute("src", "icons/heart.svg");
     var like = document.createElement("p");
-    like.innerHTML=pomArray[i].likes;
-    //divColumn.append(like);
+    like.innerHTML = pomArray[i].likes;
     lastDiv.append(icon);
     lastDiv.append(like);
     divColumn.append(lastDiv);
     document.getElementById("row").appendChild(divColumn);
   }
+  count = count + 4;
+  amount = amount + 4;
 }
 
-function imgClick(element)
-{
-    var img=document.createElement("img");    
-    var path=element.src;
-    img.setAttribute("src",path);
-    var divModal = document.createElement("div");
-    divModal.setAttribute("id","myModal");
-    divModal.className="modal";
+function modalClick(element) {
+  var modal = document.getElementById("modal");
+  modal.style.display = "block";
+  var img = document.getElementById("image");
+  var source = element.getElementsByClassName("imageClass")[0].src;
+  img.setAttribute("src", source);
+  var profileImg = document.getElementById("profileImg");
+  source = element.getElementsByClassName("profile")[0].src;
+  profileImg.setAttribute("src", source);
+  var pName = document.getElementById("pName");
+  var text = element.getElementsByClassName("name")[0].innerHTML;
+  pName.innerHTML = text;
+  var pDate = document.getElementById("pDate");
+  pDate.innerHTML = element.getElementsByClassName("date")[0].innerHTML;
+  var imgLogo = document.getElementById("logo");
+  source = element.getElementsByClassName("insta")[0].src;
+  imgLogo.setAttribute("src", source);
+  var pCaption = document.getElementById("pCaption");
+  pCaption.innerHTML = element.getElementsByClassName("caption")[0].innerHTML;
+  var likes = document.getElementById("like");
+  likes.innerHTML = element.getElementsByClassName("likes")[0].innerHTML;
 }
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
